48127e17af305e5d85a103a742db3b8d3571ec17
sisjuy <a0917589225@gmail.com>
2021-11-27 21:19:20 +0800

    11/27 col bar+nav href

--- fin_bert_web/catalog/views.py
+++ fin_bert_web/catalog/views.py
@@ -11,15 +11,17 @@ import os
 
     
 def report(request):
+    if request.method == "POST":
+        module_dir = os.path.dirname(__file__)
+        company = request.POST.get("company")
+        year = request.POST.get("year")
+        filename = company + '.txt'
+        file_path = os.path.join(module_dir,"dataset",year,filename)   #full path to text.
+        data_file = open(file_path , 'r')       
+        data = data_file.read()
+        context = {'rooms': data}
+        return render(request, 'report.html',context)
     
-    module_dir = os.path.dirname(__file__)
-    company = request.POST.get("company")
-    year = request.POST.get("year")
-    filename = company + '.txt'
-    file_path = os.path.join(module_dir,"dataset",year,filename)   #full path to text.
-    data_file = open(file_path , 'r')       
-    data = data_file.read()
-    context = {'rooms': data}
-    return render(request, 'report.html',context)
+    return render(request, 'report.html')
 
 
