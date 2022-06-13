695238fc75497c5f9ad50ea2d18f0335d636ed68
sisjuy <a0917589225@gmail.com>
2022-06-14 00:12:53 +0800

    home page: report.html, yearchange button debug

--- fin_bert_web/catalog/urls.py
+++ fin_bert_web/catalog/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('report', views.report, name='report')
+    path('', views.report, name='report')
+    #path('report', views.report, name='report')
 ]
 
 
