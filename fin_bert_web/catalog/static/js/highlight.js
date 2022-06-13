3156cee60c03b8aefffa3b1642906244f4ed7ccd
sisjuy <a0917589225@gmail.com>
2022-06-14 00:39:15 +0800

    classify js file

--- fin_bert_web/catalog/static/js/highlight.js
+++
@@ -1,5 +0,0 @@
-let str = document.getElementById("str");
-let result;
-result = str.innerHTML.replace(/Lorem Ipsum/gi, `<span class="my-span">Lorem Ipsum</span>`);
-str.innerHTML = result;
-console.log(str);
