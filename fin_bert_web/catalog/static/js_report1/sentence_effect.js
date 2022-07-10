4c47e1df1b258a8543a2b746e4de9147ee925833
sisjuy <a0917589225@gmail.com>
2022-07-10 22:19:34 +0800

    0710 report1 update

--- fin_bert_web/catalog/static/js_report1/sentence_effect.js
+++ fin_bert_web/catalog/static/js_report1/sentence_effect.js
@@ -1,4 +1,45 @@
+function scrollToElm(container, elm, duration){
+    var pos = getRelativePos(elm);
+    scrollTo( container, pos.top , 0.87);  // duration in seconds
+}
+function getRelativePos(elm){
+    var pPos = elm.parentNode.getBoundingClientRect(), // parent pos leftreport
+    cPos = elm.getBoundingClientRect(); // target pos senb
 
+    winy = window.pageYOffset;
+    //console.log(winy)
+    pos = {};
+
+    pos.top    = cPos.top    - pPos.top + elm.parentNode.scrollTop,
+    pos.right  = cPos.right  - pPos.right,
+    pos.bottom = cPos.bottom - pPos.bottom,
+    pos.left   = cPos.left   - pPos.left;
+
+
+    return pos;
+}
+function scrollTo(element, to, duration, onDone) {
+    var start = element.scrollTop,
+    change = to - start,
+    startTime = performance.now(),
+    val, now, elapsed, t;
+
+    function animateScroll(){
+        now = performance.now();
+        elapsed = (now - startTime)/1000;
+        t = (elapsed/duration);
+
+        element.scrollTop = start + change * easeInOutQuad(t);
+
+        if( t < 1 )
+            window.requestAnimationFrame(animateScroll);
+        else
+            onDone && onDone();
+    };
+
+    animateScroll();
+}
+function easeInOutQuad(t){ return t<.5 ? 2*t*t : -1+(4-2*t)*t };
 function click_a(ele){
 /* --------------------------------------------*/
 /* jump to corresponding senA if clicking senB */
