4e08a61aac458988ae693dfd77e871b720574a88
sisjuy <a0917589225@gmail.com>
2022-07-17 19:32:38 +0800

    0717 layout design

--- fin_bert_web/catalog/static/js/yearchange.js
+++ fin_bert_web/catalog/static/js/yearchange.js
@@ -2,7 +2,7 @@ function anchor_submit_left(){
     //leftsubmit()
     var curryear = localStorage.getItem("yearnow")
     var company = localStorage.getItem("companynow")
-    var item = localStorage.getItem("itemnow")
+    var item_curr = localStorage.getItem("lastclick_reportitem_curr")
     var year = (parseInt(curryear) - 1).toString()
     if (curryear=="2012"|curryear==null){
         //alert("AFDSFASDF")
@@ -11,10 +11,11 @@ function anchor_submit_left(){
     else{
         document.getElementById("left_year").setAttribute('value',year); 
         document.getElementById("left_company").setAttribute('value',company); 
-        document.getElementById("left_item").setAttribute('value',item); 
+        document.getElementById("left_item").setAttribute('value',item_curr); 
         //localstorage has to change
-        localStorage.setItem("yearnow",year)    
-        document.getElementById("leftinfo_item").innerHTML = item
+        localStorage.setItem("yearnow",year)   
+        document.getElementById("reportyear_curr").innerHTML = year
+        document.getElementById("reportyear_last").innerHTML = (parseInt(year) - 1).toString()
         //document.getElementById("rightinfo_item").innerHTML = item
         //submit
         document.getElementById("leftform").setAttribute('method',"post");
@@ -27,7 +28,7 @@ function anchor_submit_right(){
     //rightsubmit()
     var curryear = localStorage.getItem("yearnow")
     var company = localStorage.getItem("companynow")
-    var item = localStorage.getItem("itemnow")
+    var item_curr = localStorage.getItem("lastclick_reportitem_curr")
     var year = (parseInt(curryear) + 1).toString()
     //console.log("hereeeeeee")
     if (curryear=="2018" | curryear==null){
@@ -39,7 +40,8 @@ function anchor_submit_right(){
         document.getElementById("right_item").setAttribute('value',item); 
         //localstorage has to change
         localStorage.setItem("yearnow",year)
-        document.getElementById("leftinfo_item").innerHTML = item
+        document.getElementById("reportyear_curr").innerHTML = (parseInt(curryear) + 1).toString()
+        document.getElementById("reportyear_last").innerHTML = curryear
         //document.getElementById("rightinfo_item").innerHTML = item
         //submit
         document.getElementById("rightform").setAttribute('method',"post");
